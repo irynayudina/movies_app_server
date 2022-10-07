a95d9473860d692815c465244216eaa0160f443c
Iryna Yudina <yudinaira4444@gmail.com>
2022-10-07 19:01:59 +0300

    make it better

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const FilmController = require('./controllers/FilmController')
 
 const app = express();
 var corsOptions = {
-    origin: `https://movies-catalog-app.herokuapp.com`,
+    origin: `http://localhost:3000/`,
     // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(cors(corsOptions));
