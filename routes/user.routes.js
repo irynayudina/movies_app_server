d38376313543350569631878566ac777e8b92763
Iryna Yudina <yudinaira4444@gmail.com>
2023-07-13 22:57:08 +0300

    nodemailer

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -24,37 +24,25 @@ function genRandPass() {
     return randpass;
 }
 async function sendEmail(email, pw) {
-    nodemailer.createTestAccount((err, account) => {
-        if (err) {
-            console.error("Failed to create a testing account. " + err.message);
-            return process.exit(1);
-        }
-        console.log("Credentials obtained, sending message...");
-        let transporter = nodemailer.createTransport({
-            host: account.smtp.host,
-            port: account.smtp.port,
-            secure: account.smtp.secure,
-            auth: {
-                user: account.user,
-                pass: account.pass,
-            },
-        });
-        let message = {
-            from: "Sender Name <sender@example.com>",
-            to: "Recipient <yudinaira4444@gmail.com>",
-            subject: "Nodemailer is unicode friendly ✔",
-            text: "Hello to myself!",
-            html: "<p><b>Hello</b> to myself!</p>",
-        };
-        transporter.sendMail(message, (err, info) => {
-            if (err) {
-                console.log("Error occurred. " + err.message);
-                return process.exit(1);
-            }
-            console.log("Message sent: %s", info.messageId);
-            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-        });
+    const smtpTransport = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
     });
+    const mailOptions = {
+      to: email,
+      from: "Movies App",
+      subject: "Password Reset",
+      text:
+        "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
+        "New password is: "+
+        pw +
+        "\n\n",
+    };
+
+    await smtpTransport.sendMail(mailOptions);
 }
 
 function generateAccessToken(user) {
